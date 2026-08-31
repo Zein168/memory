6e360e1533accb32abdf9b8a9ce6990b45758de7
Zein168 <zein.9494@outlook.com>
2026-08-31 16:29:30 +0200

    feat: add option selection with radius and TypeScript

--- src/settings.ts
+++ src/settings.ts
@@ -1,1 +1,19 @@
 import './settings.scss'
+
+const options = document.querySelectorAll<HTMLParagraphElement>(
+  ".settings__option"
+);
+
+options.forEach((option) => {
+  option.addEventListener("click", () => {
+    const group = option.closest(".settings__group");
+
+    if (!group) return;
+
+    group
+      .querySelectorAll(".settings__option")
+      .forEach((item) => item.classList.remove("active"));
+
+    option.classList.add("active");
+  });
+});
