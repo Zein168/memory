52cfeacdf3579b40f9be60ef4431d8064bc914b0
Zein168 <zein.9494@outlook.com>
2026-09-01 10:38:16 +0200

    feat: add card count selection to game

--- src/game.ts
+++ src/game.ts
@@ -1,1 +1,27 @@
 import './game.scss'
+
+const cardCount = Number(localStorage.getItem("cardCount"));
+const board = document.querySelector<HTMLDivElement>(".game__board");
+
+if (!board) {
+  throw new Error("Game board not found");
+}
+
+for (let i = 0; i < cardCount; i++) {
+  const card = document.createElement("div");
+  card.classList.add("game__card");
+
+  board.appendChild(card);
+}
+
+if (cardCount === 16) {
+  board.style.gridTemplateColumns = "repeat(4, 1fr)";
+}
+
+if (cardCount === 24) {
+  board.style.gridTemplateColumns = "repeat(6, 1fr)";
+}
+
+if (cardCount === 36) {
+  board.style.gridTemplateColumns = "repeat(6, 1fr)";
+}
--- src/settings.ts
+++ src/settings.ts
@@ -30,3 +30,21 @@ options.forEach((option) => {
     }
   });
 });
+
+
+const startButton = document.querySelector<HTMLButtonElement>(".start-button");
+startButton?.addEventListener("click", () => {
+  const selectedOption = document.querySelector<HTMLParagraphElement>(
+    ".settings__board-size .settings__option.active"
+  );
+
+  if (!selectedOption) return;
+
+  const cardCount = Number(
+    selectedOption.textContent?.replace("cards", "").trim()
+  );
+
+  localStorage.setItem("cardCount", String(cardCount));
+
+  window.location.href = "./game.html";
+});
